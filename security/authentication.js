71c1a7fa3addb521e0a82f5325a791253c6f50c6
ObidovZoftify <123758376+ObidovZoftify@users.noreply.github.com>
2024-04-13 15:38:59 +0500

    fix: authentication

--- security/authentication.js
+++ security/authentication.js
@@ -2,15 +2,14 @@ const { pool } = require("../utils/connection.db")
 const { verifyBearerToken } = require("./jwt.token")
 
 exports.authenticate = async (req, res, next) => {
-    if (!req.headers['authorization']) return res.status(400).json({
+    if (!req.headers['authorization']) return res.status(500).json({
         result: 'SECURITY CREDENTIAL WAS NOT PROVIDED'
     })
     try {
-        const payload = await verifyBearerToken(req.headers['authorization'])
-        console.log(payload)
+        const auth = req.headers['authorization'].split(' ').pop() // SPLIT TOKEN FROM BEARER
+        const payload = await verifyBearerToken(auth)
         const [token] = await pool.query('SELECT * FROM TOKENS WHERE user_id = ?', payload.id)
-        const [blackListToken] = await pool.query('SELECT * FROM BLOCKED_TOKENS WHERE BEARER_TOKEN = ?', req.headers['authorization'])
-        console.log(blackListToken)
+        const [blackListToken] = await pool.query('SELECT * FROM BLOCKED_TOKENS WHERE BEARER_TOKEN = ?', auth)
         if(JSON.parse(JSON.stringify(token)).length === 0 || JSON.parse(JSON.stringify(blackListToken)).length !== 0) throw new Error
         res.locals.payload = payload;
         next()
